71e1b7e3501ed43aa72e43242c5c5d0d0d0c99d1
JordanEckford <jordan.eckford@gmail.com>
2023-10-04 15:26:34 +0100

    added solutions and testing for task 12 GET/api/articles/:article_id (added comment_count)

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,7 +1,8 @@
 const db = require("../db/connection");
 
 exports.fetchArticleByID = (articleID) => {
- const query = `SELECT * FROM articles WHERE article_id = $1;`;
+ const query =
+  "SELECT articles.author, title, articles.body, articles.article_id, topic, articles.created_at, articles.votes, article_img_url, COUNT(comments.article_id) AS comment_count FROM articles FULL OUTER JOIN comments ON comments.article_id = articles.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id ORDER BY articles.created_at DESC;";
  return db.query(query, [articleID]).then(({ rows }) => {
   if (rows.length === 0) {
    return Promise.reject({ status: 404, msg: "article does not exist" });
