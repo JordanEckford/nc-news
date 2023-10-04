61f154c6f80beed255d3cdbd6f09971343442c80
JordanEckford <jordan.eckford@gmail.com>
2023-10-04 11:37:31 +0100

    added solutions and testing to task 10 - GET /api/users

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -317,3 +317,22 @@ describe("DELETE: /api/comments/:comment_id", () => {
    });
  });
 });
+describe("GET /api/users", () => {
+ test("should respond with a 200 status code and array of objects containing user info", () => {
+  return request(app)
+   .get("/api/users")
+   .expect(200)
+   .then(({ body }) => {
+    expect(body.users.length).toBe(4);
+    body.users.forEach((user) => {
+     expect(user).toEqual(
+      expect.objectContaining({
+       username: expect.any(String),
+       name: expect.any(String),
+       avatar_url: expect.any(String),
+      })
+     );
+    });
+   });
+ });
+});
